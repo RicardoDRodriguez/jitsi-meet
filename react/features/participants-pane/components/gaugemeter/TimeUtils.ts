7d5d2ab7656419a1f25db49a28c73524b113cb83
RicardoDRodriguez <ricardo.domingos@gmail.com>
2025-04-04 17:14:03 -0300

    Inserindo um objeto para analisar saidas e entradas-Corrigindo Erros (6)

--- react/features/participants-pane/components/gaugemeter/TimeUtils.ts
+++ react/features/participants-pane/components/gaugemeter/TimeUtils.ts
@@ -16,6 +16,11 @@ export function formatTimeFromMilliseconds(milliseconds: number): string {
         const minutos = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
         const segundos = Math.floor((milliseconds % (1000 * 60)) / 1000);
 
+        console.log(' FormatTimeFromMilliseconds millisegundos:',milliseconds);
+        console.log(' FormatTimeFromMilliseconds horas:',horas);
+        console.log(' FormatTimeFromMilliseconds minutos:',minutos);
+        console.log(' FormatTimeFromMilliseconds segundos:',segundos);
+
         // Formata sempre com horas, minutos e segundos
         return `${horas}h ${minutos}m ${segundos}s`;
     } catch (error) {
