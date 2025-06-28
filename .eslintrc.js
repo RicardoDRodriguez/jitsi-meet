a4d09315bc8588515e195bab1eb451fff457c043
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2025-06-28 14:33:10 +0000

    Inserindo as extensões pretty e ESLINT

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,46 @@
+// .eslintrc.js
 module.exports = {
-    'extends': [
-        '@jitsi/eslint-config'
-    ],
-    'ignorePatterns': [ '*.d.ts' ]
+  // O ambiente onde o código vai rodar (browser, node, etc.)
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true, // Se você usar Jest para testes
+  },
+  // Conjuntos de regras pré-configuradas que você pode estender
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+
+  ],
+  // O parser que vai analisar o código
+  parser: '@typescript-eslint/parser',
+  // Opções específicas do parser
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Habilita a análise de JSX
+    },
+    ecmaVersion: 'latest', // Usa a versão mais recente do ECMAScript
+    sourceType: 'module', // Permite o uso de 'imports'
+  },
+  // Plugins que adicionam novas regras
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+  // Aqui você pode sobrescrever ou adicionar regras específicas
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Regra desativada para React 17+
+    'react/prop-types': 'off', // Desativado pois usamos TypeScript para tipos
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  // Configurações que podem ser compartilhadas entre as regras
+  settings: {
+    react: {
+      version: 'detect', // Detecta automaticamente a versão do React
+    },
+  },
 };
